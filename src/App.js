ad21b701865122b5fd38014fb1f1a4cf142a8dbb
Aaron Fowlkes <afowlkes21@gmail.com>
2022-07-19 17:37:56 -0400

    added dropdown menu with character images

--- src/App.js
+++ src/App.js
@@ -2,26 +2,21 @@ import { useState } from 'react'
 // import React from 'react'
 import './App.css'
 
-import logo from './logo.svg';
 import './App.css';
+import MainMenu from './components/MainMenu'
+import CreateACharacter from './components/CreateACharacter'
+
 
 function App() {
+  const [screen, setScreen] = useState('main')
 
   return (
-    <div className="screen">
-      <video autoPlay muted loop id="myVideo">
-        <source src={"public/fireVid.mov"} type="video/mp4" />
-      </video>
-      <div className="" id="fs-overlay">
-      <img 
-      id="header-img"
-      src={'https://1000logos.net/wp-content/uploads/2021/08/Dungeons-and-Dragons-Logo-2014.png'}
-      />
-      <div className="menu-links">
-        <h3>{'Create a Character'}</h3>
-        <h3>{'About Dungeons & Dragrons'}</h3>
-      </div>
-      </div>
+    <div>
+      {
+        (screen === 'main') ? 
+        <MainMenu setScreen={setScreen}/> :
+        <CreateACharacter setScreen={setScreen} />
+      }
     </div>
     )    
 }
